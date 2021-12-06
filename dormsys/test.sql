38b3f15a41ffcafd4a68bc5d8035131f9db61ec1
Canwaiting <455021735@qq.com>
2021-12-06 13:05:18 +0800

    1

---
+++ dormsys/test.sql
@@ -0,0 +1,17 @@
+/*create the table*/
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`(
+    `name` varchar(30) DEFAULT NULL, 
+    `password` varchar(30) DEFAULT NULL 
+)ENGINE=innoDB DEFAULT CHARSET=utf8;
+
+/*insert the data*/
+insert into user
+(name,password)
+VALUES
+("root","12345");
+
+insert into user
+(name,password)
+VALUES
+    ("canwaiting","12345");
